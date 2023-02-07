2266c8dbf2995aaaeb655630e7bd3e91c8fd46d7
zhalgas-seidazym <124429967+zhalgas-seidazym@users.noreply.github.com>
2023-02-07 20:38:02 +0600

    Shape.java
    added object shape

---
+++ problem-B/Shape.java
@@ -0,0 +1,33 @@
+public class Shape {
+    private String color;
+    private boolean filled;
+
+//----------Constructor-------------
+    public Shape(){
+        this("red" , true);
+    }
+    public Shape(String color, boolean filled){
+        this.color = color;
+        this.filled = filled;
+    }
+
+//----------Getter------------------
+    public String getColor(){
+        return this.color;
+    }
+    public boolean isFilled(){
+        return this.filled;
+    }
+
+//----------Setter-----------------
+    public void setColor(String color){
+        this.color = color;
+    }
+    public void setFilled(boolean filled){
+        this.filled = filled;
+    }
+//----------ToString---------------
+    public String toString(){
+        return "Shape[color=" + this.color + ",filled=" + this.filled + "]";
+    }
+}
