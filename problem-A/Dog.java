467f634fded745e63babef84bd42b428d61f28e6
zhalgas-seidazym <124429967+zhalgas-seidazym@users.noreply.github.com>
2023-02-07 22:31:40 +0600

    Update Dog.java

--- problem-A/Dog.java
+++ problem-A/Dog.java
@@ -1,18 +1,19 @@
 public class Dog extends Mammal {
-    //------------Constructor---------
-        public Dog(String name){
+ //------------Constructor---------
+     public Dog(String name){
             super(name);
         }
-    //-----------Method------------
-        public void greets(){
-            System.out.println("Woof");
-        }
-        public void greets(Dog dog){
-            System.out.println("Woooof");
-        }
-    //-----------ToString-----------
-        @Override
-        public String toString(){
-            return "Dog[" + super.toString() + "]";
-        }
+//-----------Method------------
+    public void greets(){
+        System.out.println("Woof");
     }
+    public void greets(Dog dog){
+        System.out.println("Woooof");
+    }
+//-----------ToString-----------
+    @Override
+    public String toString(){
+        return "Dog[" + super.toString() + "]";
+    }
+}
+    
